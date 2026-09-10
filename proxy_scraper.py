32ea9a462c9ca6d6111ace55a991698b54c01d6d
eshrusv <amanparkar30+git@gmail.com>
2026-09-10 10:02:15 +0530

    Update proxy_scraper.py

--- proxy_scraper.py
+++ proxy_scraper.py
@@ -9,8 +9,8 @@ SETUP:
   pip install aiohttp aiofiles colorama tqdm
 
 CONFIG (edit below):
-  TELEGRAM_BOT_TOKEN = "your_bot_token"
-  TELEGRAM_CHAT_ID   = "your_chat_id"
+  TELEGRAM_BOT_TOKEN = "8842957202:AAF7IIfnBjhTtTd2tc2dm-z_1MrKHiA5-S4"
+  TELEGRAM_CHAT_ID   = "8189708860"
   TARGET_COUNT       = 500       # proxies needed before sending
   MAX_LATENCY_MS     = 200       # max allowed latency in ms
   WORKERS            = 2000      # concurrent check workers (tune to your RAM)
